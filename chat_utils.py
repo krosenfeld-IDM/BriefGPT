d310f0d9641e215c115ab7beeb2610495b6e49a2
Katherine <krosenf@gmail.com>
2025-01-18 09:11:31 -0800

    us uv and more fixes

--- chat_utils.py
+++ chat_utils.py
@@ -3,7 +3,6 @@ import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 
 
